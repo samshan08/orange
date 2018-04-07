b0d73055a88c8e7a452e098aac683c178148af6b
samshan08 <samshan08@126.com>
2018-04-07 16:43:43 +0800

    six god calc

--- FortuneTeller/app/src/main/java/org/sam/fortuneteller/data/FortuneParser.java
+++ FortuneTeller/app/src/main/java/org/sam/fortuneteller/data/FortuneParser.java
@@ -159,6 +159,70 @@ public class FortuneParser {
         }
     }
 
+    public static String[] getSixGod(String dayTop) {
+        String[] arrayOfString4 = new String[7];
+        arrayOfString4[0] = "";
+        if ((dayTop.equals("甲")) || (dayTop.equals("乙")))
+        {
+            arrayOfString4[1] = "龙";
+            arrayOfString4[2] = "雀";
+            arrayOfString4[3] = "勾";
+            arrayOfString4[4] = "蛇";
+            arrayOfString4[5] = "虎";
+            arrayOfString4[6] = "玄";
+        }
+        if ((dayTop.equals("丙")) || (dayTop.equals("丁")))
+        {
+            arrayOfString4[1] = "雀";
+            arrayOfString4[2] = "勾";
+            arrayOfString4[3] = "蛇";
+            arrayOfString4[4] = "虎";
+            arrayOfString4[5] = "玄";
+            arrayOfString4[6] = "龙";
+        }
+        if (dayTop.equals("戊"))
+        {
+            arrayOfString4[1] = "勾";
+            arrayOfString4[2] = "蛇";
+            arrayOfString4[3] = "虎";
+            arrayOfString4[4] = "玄";
+            arrayOfString4[5] = "龙";
+            arrayOfString4[6] = "雀";
+        }
+        if (dayTop.equals("己"))
+        {
+            arrayOfString4[1] = "蛇";
+            arrayOfString4[2] = "虎";
+            arrayOfString4[3] = "玄";
+            arrayOfString4[4] = "龙";
+            arrayOfString4[5] = "雀";
+            arrayOfString4[6] = "勾";
+        }
+        if ((dayTop.equals("庚")) || (dayTop.equals("辛")))
+        {
+            arrayOfString4[1] = "虎";
+            arrayOfString4[2] = "玄";
+            arrayOfString4[3] = "龙";
+            arrayOfString4[4] = "雀";
+            arrayOfString4[5] = "勾";
+            arrayOfString4[6] = "蛇";
+        }
+        if ((dayTop.equals("壬")) || (dayTop.equals("癸")))
+        {
+            arrayOfString4[1] = "玄";
+            arrayOfString4[2] = "龙";
+            arrayOfString4[3] = "雀";
+            arrayOfString4[4] = "勾";
+            arrayOfString4[5] = "蛇";
+            arrayOfString4[6] = "虎";
+        }
+        String[] actual = new String[6];
+        for (int i = 6; i > 0; --i) {
+            actual[6 - i] = arrayOfString4[i];
+        }
+        return actual;
+    }
+
     public static void main(String[] args) {
         for (int i = 1; i < 100; ++i) {
             String value = parsePreValue(String.valueOf(i));
